1101f3a7d806e18d5125f9526178cdef9f6fc01b
essential-software-inc <contactrhudio@gmail.com>
2026-09-25 16:48:55 +0100

    Rename appName from 'Builder' to 'Bluebook'

--- capacitor.config.free.ts
+++ capacitor.config.free.ts
@@ -5,7 +5,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 // painful to rename later. appName is the label under the home-screen icon.
 const config: CapacitorConfig = {
   appId: 'com.essentialsoftware.notebuilder',
-  appName: 'Builder',
+  appName: 'Bluebook',
   webDir: 'www',
   server: {
     androidScheme: 'https'
